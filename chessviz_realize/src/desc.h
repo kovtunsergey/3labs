83c6931061f129375cd043f0c22d061a9a66bbed
Ковтун Сергей Алексеевич <ip714s12@mwp07.csc.local>
2018-05-17 14:23:50 +0700

    reppozitoriy

---
+++ chessviz_realize/src/desc.h
@@ -0,0 +1,4 @@
+#ifndef h_plain
+#define h_plain
+void scanh(int &x1, int &y1, int &x2, int &y2,char desk[][9],int flag);
+#endif
---
+++ chessviz_realize/src/print_board.h
@@ -0,0 +1,4 @@
+#ifndef h_board_print_plain
+#define h_board_print_plain
+void printdesk (char desk[9][9]);
+#endif
